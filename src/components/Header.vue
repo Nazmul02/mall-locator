e9247ceffe66873354c155c347f96743f0d9a118
nazmul02 <nazmul.abedin02@gmail.com>
2024-09-08 21:06:28 +0800

    Mall locator app

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="bg-blue-600 p-4 text-white">
+    <h1 class="text-center text-2xl font-bold">Mall Locator App</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style scoped>
+</style>
